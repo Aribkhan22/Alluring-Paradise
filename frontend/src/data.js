185d6e9db1fa91389dfb0c8e676022b10249c70c
Aribkhan22 <68198678+Aribkhan22@users.noreply.github.com>
2021-09-23 02:02:07 +0530

    First commit

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Beard Oil',
+            category: 'Mens Beauty Product',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Beardoo',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'mast oil hai bhai',
+
+        },
+        {
+            _id: '2',
+            name: 'lipstick',
+            category: 'Mens Beauty Product',
+            image: '/images/p2.jpg',
+            price: 120,
+            brand: 'Beardoo',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'mast oil hai bhai',
+
+        },
+        {
+            _id: '3',
+            name: 'Mascara',
+            category: 'Mens Beauty Product',
+            image: '/images/p3.jpg',
+            price: 120,
+            brand: 'Beardoo',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'mast oil hai bhai',
+
+        },
+        {
+            _id: '4',
+            name: 'Eye liner',
+            category: 'Mens Beauty Product',
+            image: '/images/p4.jpg',
+            price: 120,
+            brand: 'Beardoo',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'mast oil hai bhai',
+
+        },
+        {
+            _id: '5',
+            name: 'Powder',
+            category: 'Mens Beauty Product',
+            image: '/images/p5.jpg',
+            price: 120,
+            brand: 'Beardoo',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'mast oil hai bhai',
+
+        },
+        {
+            _id: '6',
+            name: 'Hair colour',
+            category: 'Mens Beauty Product',
+            image: '/images/p6.jpg',
+            price: 120,
+            brand: 'Beardoo',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'mast oil hai bhai',
+
+        },
+
+    ],
+};
+export default data;
